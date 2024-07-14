b8765a3bf253dbd025c50621e220ac2fe9288cd8
Ruihang Zhang <ruihang.zhang@mail.utoronto.ca>
2024-07-14 13:37:57 -0400

    completed decoder

--- diffusion/stable-diffusion/sd/decoder.py
+++ diffusion/stable-diffusion/sd/decoder.py
@@ -60,3 +60,75 @@ class VAE_ResidualBlock(nn.Module):
         x = self.conv_2(x)
 
         return x + self.residual_layer(residue) # making sure x and residue have the same size when in_channels != out_channels
+    
+class VAE_Decoder(nn.Sequential):
+    def __init__(self):
+        super().__init__(
+            nn.Conv2d(4, 4, kernel_size=1, padding=0),
+
+            nn.Conv2d(4, 512, kernel_size=3, padding=1), 
+
+            VAE_ResidualBlock(512, 512),
+
+            VAE_AttentionBlock(512),
+
+            VAE_ResidualBlock(512, 512),
+
+            VAE_ResidualBlock(512, 512),
+
+            VAE_ResidualBlock(512, 512),
+
+            # (batch_size, num_features = 512, height/8 = 512/8, width/8 = 512/8) -> (batch_size, num_features = 512, height/8 = 512/8, width/8 = 512/8)
+            VAE_ResidualBlock(512, 512),
+
+            # (batch_size, num_features = 512, height/8 = 512/8, width/8 = 512/8) -> (batch_size, num_features = 512, height/4 = 512/4, width/4 = 512/4)
+            nn.Upsample(scale_factor=2), 
+
+            nn.Conv2d(512, 512, kernel_size=3, padding=1),
+
+            VAE_ResidualBlock(512, 512),
+
+            VAE_ResidualBlock(512, 512),
+
+            VAE_ResidualBlock(512, 512),
+
+            # (batch_size, num_features = 512, height/4 = 512/4, width/4 = 512/4) -> (batch_size, num_features = 512, height/2 = 512/2, width/2 = 512/2)
+            nn.Upsample(scale_factor=2), 
+
+            nn.Conv2d(512, 512, kernel_size=3, padding=1),
+
+            VAE_ResidualBlock(512, 256),
+
+            VAE_ResidualBlock(256, 256),
+
+            VAE_ResidualBlock(256, 256),
+            
+            # (batch_size, num_features = 256, height/2 = 512/2, width/2 = 512/2) -> (batch_size, num_features = 256, height, width)
+            nn.Upsample(scale_factor=2), 
+
+            nn.Conv2d(256, 256, kernel_size=3, padding=1),
+
+            VAE_ResidualBlock(256, 128),
+
+            VAE_ResidualBlock(128, 128),
+
+            VAE_ResidualBlock(128, 128),
+
+            # (32 groups, 128 features): dividing 128 features into groups of 32
+            nn.GroupNorm(32, 128),
+
+            nn.SiLU(),
+
+            # (batch_size, num_features = 128, height, width) -> (batch_size, num_features = 3, height, width)
+            nn.Conv2d(128, kernel_size=3, padding=1)
+        )
+    def forward(self, x:torch.Tensor) -> torch.Tensor:
+        # x: (batch_size, 4, height/8, width/8)
+
+        x /= 0.18215
+
+        for module in self:
+            x = module(x)
+
+        # (batch_size, 3, height, width)
+        return x
