9f83a65029696aad21ff26a43cad42f967d2b926
Ruihang Zhang <ruihang.zhang@mail.utoronto.ca>
2024-07-15 16:11:01 -0400

    added cross attention

--- diffusion/stable-diffusion/sd/attention.py
+++ diffusion/stable-diffusion/sd/attention.py
@@ -52,3 +52,47 @@ class SelfAttention(nn.Module):
 
         # (batch_size, seq_len, dim)
         return output
+    
+class CrossAttention(nn.Module):
+    def __init__(self, n_heads: int, d_embed: int, d_cross: int, in_proj_bias=True, out_proj_bias=True):
+        super().__init__()
+        self.q_proj = nn.Linear(d_embed, d_embed, bias=in_proj_bias)
+        self.k_proj = nn.Linear(d_cross, d_embed, bias=in_proj_bias)
+        self.v_proj = nn.Linear(d_cross, d_embed, bias=in_proj_bias)
+        self.out_proj = nn.Linear(d_embed, d_embed, bias=out_proj_bias)
+        self.n_heads = n_heads
+        self.d_head = d_embed // n_heads
+    
+    def forward(self, x, y):
+        # x(latent): (batch_size, seq_len_Q, dim_Q)
+        # y(context): (batch_size, seq_len_KV, dim_KV) = (batch_size, 77, 768)
+
+        input_shape = x.shape
+        batch_size, seq_len, d_embed = input_shape
+        
+        interim_shape = (batch_size, -1, self.n_heads, self.d_head)
+
+        # multiply query by Wq
+        q = self.q_proj(x)
+        k = self.k_proj(y)
+        v = self.k_proj(y)
+
+        q = q.view(interim_shape.transpose(1, 2))
+        k = k.view(interim_shape.transpose(1, 2))
+        v = v.view(interim_shape.transpose(1, 2))
+
+        weight = q @ k.transpose
+
+        weight /= math.sqrt(self.d_head)
+
+        weight = F.softmax(weight, dim=-1)
+
+        output = weight @ v
+
+        output = output.transpose(1, 2).continous()
+
+        output = output.view(input_shape)
+
+        output = self.out_proj(output)
+
+        return output
